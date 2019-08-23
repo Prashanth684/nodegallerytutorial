832a2c08e6191f09d334fa064e09bb64d7988aef
Prashanth Sundararaman <psundara@redhat.com>
2019-08-23 11:31:12 -0400

    take dropbox vars from env

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 
+const process = require('process');
+
 module.exports = {
 	DBX_API_DOMAIN: 'https://api.dropboxapi.com',
 	DBX_OAUTH_DOMAIN: 'https://www.dropbox.com',
 	DBX_OAUTH_PATH: '/oauth2/authorize',
 	DBX_TOKEN_PATH: '/oauth2/token',
-  DBX_APP_KEY:'<appkey_in_dropbox_console>',
-  DBX_APP_SECRET:'<appsecret_in_dropbox_console>', 
-	OAUTH_REDIRECT_URL:"https://dbx-test.apps.xyz.com:443/oauthredirect",
+  DBX_APP_KEY: process.env.APP_KEY,
+  DBX_APP_SECRET: process.env.APP_SECRET, 
+	OAUTH_REDIRECT_URL: process.env.DBX_URL,
 	DBX_LIST_FOLDER_PATH:'/2/files/list_folder',
 	DBX_LIST_FOLDER_CONTINUE_PATH:'/2/files/list_folder/continue',
 	DBX_GET_TEMPORARY_LINK_PATH:'/2/files/get_temporary_link',
